270a252b287b06f6a531b180574935837aa5ce77
Rodolphe R. CLEMENCEAU <RCLEMENCEAU@slts-asp.local>
2023-06-16 17:05:49 +0200

    Gestion des users

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, password=None, **Kwargs):
+    def create_user(self, email, first_name, last_name, password=None):
         if not email:
             raise ValueError("Vous devez entrer un email")
         if not first_name:
@@ -14,7 +14,7 @@ class MyUserManager(BaseUserManager):
             email=self.normalize_email(email),
             first_name=first_name,
             last_name=last_name,
-            )
+        )
         user.set_password(password)
         user.save()
         return user
@@ -33,6 +33,7 @@ class MyUserManager(BaseUserManager):
 
 class User(AbstractUser):
     username = None
+    user_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     email = models.EmailField(unique=True, max_length=255, blank=False)
@@ -41,6 +42,7 @@ class User(AbstractUser):
     is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
     objects = MyUserManager()
 
     def has_perm(slf, perm, obj=None):
--- src/softdesk/settings.py
+++ src/softdesk/settings.py
@@ -125,3 +125,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "authentication.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+
+    ),
+
+    'USER_ID_FIELD': 'user_id'
+}
